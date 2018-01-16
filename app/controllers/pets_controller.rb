d63ef65968d244ba3ed2f942f76ea597561c5074
Susan Fowler <infopro94@gmail.com>
2018-01-16 20:28:12 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,9 +15,8 @@ class PetsController < ApplicationController
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-    binding.pry
-    @owner.save
-    redirect "pets/#{@pet.id}"
+      @owner.save
+      redirect "pets/#{@pet.id}"
   end
 
   get '/pets/:id/edit' do
@@ -35,6 +34,7 @@ class PetsController < ApplicationController
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
+      binding.pry
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
